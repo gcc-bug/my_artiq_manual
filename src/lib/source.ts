d1c5c00a1bfd423455f73b08cea3aaa10f7afe7e
gcc-bug <by.gdc@outlook.com>
2025-04-25 17:40:45 +0800

    🐞 fix

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -7,10 +7,10 @@ export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
   icon(iconName) {
-    if (!iconName) return null;
+    if (!iconName) return undefined;
     if (iconName in icons) {
       return createElement(icons[iconName as keyof typeof icons]);
     }
-    return null;
+    return undefined;
   },
 });
